130c382416e5513ad4536c7b67699592d1407565
davidreyesunc <david.reyes@mi.unc.edu.ar>
2021-04-13 09:54:32 -0300

    Suma devuelve el resultado en EAX

--- suma.asm
+++ suma.asm
@@ -1,12 +1,10 @@
-; void PRE_CDECL suma( int n, int m, int * sump) POST_CDECL
+; int PRE_CDECL suma( int n, int m) POST_CDECL
 ; suma 2 enteros
 ; Parametros:
 ;   n    - 1° operando de la suma (en [ebp + 8])
-;   m    - 2° operando de la suma (en [ebp + 12])                     
-;   sump - puntero al resultado de la suma (en [ebp + 16])
+;   m    - 2° operando de la suma (en [ebp + 12])
 
 ; STACK FRAME:
-; ebp + 16      resp
 ; ebp + 12      m
 ; ebp +  8      n
 ; ebp +  4      dir retorno
@@ -16,16 +14,10 @@ segment .text
         global  suma
 
 suma:
-        enter   0,0                     ; guarda valor de ebx porque se va a utilizar
-        push    ebx               
+        enter   0,0
 
-
-        mov     eax, [ebp + 8]          ;eax = 1° operando (n)
-        add     eax, [ebp + 12]         ;eax = eax + 2° operando (m)
-
-        mov     ebx, [ebp + 16]         ;ebx = dirección sump
-        mov     [ebx], eax              ;sump = eax
-
-        pop     ebx                     ; restaura ebx
+        mov     eax, [ebp + 8]          ; eax = 1° operando (n)
+        add     eax, [ebp + 12]         ; eax = eax + 2° operando (m)
+                                        ; se retorna el valor de la suma en eax
         leave
         ret
